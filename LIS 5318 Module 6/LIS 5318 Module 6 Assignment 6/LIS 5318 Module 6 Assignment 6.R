bc931391c938e3acb36bf79cb06d8367f2952528
Kelsey Velez <kelsey@Kelseys-MBP.lan>
2024-10-07 09:27:22 -0400

    updated assignment

--- LIS 5318 Module 6/LIS 5318 Module 6 Assignment 6/LIS 5318 Module 6 Assignment 6.R
+++ LIS 5318 Module 6/LIS 5318 Module 6 Assignment 6/LIS 5318 Module 6 Assignment 6.R
@@ -5,3 +5,25 @@ install.packages("ggplot2")
 library("ggplot2")
 library(ggplot2)
 ggplot(df, aes(x = OfficerAge, y = OfficerTenure)) + geom_line(color = "blue") + labs(x = "Officer Age", y = "Officer Tenure", title = "Officer Age vs Officer Tenure - Officer Involved Shootings - 2023") + theme_minimal()
+df_summary <- df %>%
+  group_by(Subjectshot, Fatal) %>%
+  summarize(count = n())
+library(dplyr)
+df_summary <- df %>%
+  group_by(Subjectshot, Fatal) %>%
+  summarize(count = n())
+ggplot(df_summary, aes(x = SubjectShot, y = count, fill = Fatal)) +
+  geom_bar(stat = "identity", position = "dodge", alpha = 0.7) +
+  labs(x = "Subject Shot", y = "Count", title = "Officer Involved Shootings - 2023") +
+  theme_minimal()
+df <- read.csv("/Users/kelsey/Desktop/LIS 5318 Visual Analytics/JaxShootingData2023.csv")
+install.packages("ggplot2")
+library(ggplot2)
+library(dplyr)
+df_summary <- df %>%
+  group_by(Subjectshot, Fatal) %>%
+  summarize(count = n())
+ggplot(df_summary, aes(x = Subjectshot, y = count, fill = Fatal)) +
+  geom_bar(stat = "identity", position = "dodge", alpha = 0.7) +
+  labs(x = "Subject Shot", y = "Count", title = "Officer Involved Shootings - 2023") +
+  theme_minimal()
