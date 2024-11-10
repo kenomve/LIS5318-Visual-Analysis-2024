a03759525712e5763a0b41dc70d3d1ec0adfc7b4
Kelsey Velez <kelsey@Kelseys-MBP.lan>
2024-11-10 11:19:43 -0500

    added module 11 assignment

---
+++ LIS 5318 Module 11 Assignment.R
@@ -0,0 +1,18 @@
+#find bug
+tukey_multiple <- function(x) {
+  outliers <- array(TRUE,dim=dim(x))
+  for (j in 1:ncol(x))
+  {
+    outliers[,j] <- outliers[,j] && tukey.outlier(x[,j])
+  }
+  outlier.vec <- vector(length=nrow(x))
+  for (i in 1:nrow(x))
+  { outlier.vec[i] <- all(outliers[i,]) }
+
+  return(outlier.vec)
+  }
+print(tukey_multiple)
+
+return(tukey_multiple(x)
+
+tukey_multiple(x)
